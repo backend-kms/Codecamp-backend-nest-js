1a1a34b01c419cd4c9b57a6a5dbcb8641163e594
Kimminsu <kms@kristinkorea.com>
2023-05-08 15:25:17 +0900

    게시판 CRUD API

--- class/nestjs/08-05-nestjs-with-typeorm-api/src/app.module.ts
+++ class/nestjs/08-05-nestjs-with-typeorm-api/src/app.module.ts
@@ -16,7 +16,7 @@ import { Board } from './apis/boards/board.entity';
     }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
+      host: '127.0.0.1',
       port: 3306,
       username: 'root',
       password: '1215',
